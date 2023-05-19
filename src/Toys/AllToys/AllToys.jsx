3d684a0dd5d929f7b24a8945ec7e00dd4ad6ef6d
AbulFozolJumman <abulfozoljumman@gmail.com>
2023-05-19 16:15:10 +0600

    Get data from MongoDb and showing it in All toys page done

--- src/Toys/AllToys/AllToys.jsx
+++ src/Toys/AllToys/AllToys.jsx
@@ -1,9 +1,17 @@
+import { useLoaderData } from "react-router-dom";
+import AllToyCard from "./AllToyCard";
 
 
 const AllToys = () => {
+    const allToys = useLoaderData();
     return (
-        <div>
-            All Toys
+        <div className="max-w-7xl mx-auto my-8">
+            <h1 className="text-4xl text-center font-semibold mb-6">All Toys</h1>
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                {allToys?.map((toy, index) => (
+                    <AllToyCard key={index} toy={toy}></AllToyCard>
+                ))}
+            </div>
         </div>
     );
 };
